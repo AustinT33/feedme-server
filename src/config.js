6269c4a13d03c22764291b4a7094dd50778b5ece
AustinT33 <austintumlinson@gmail.com>
2020-01-09 16:30:43 -0600

    changes env DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/restaurants',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/restaurants',
     TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://postgres@localhost/restaurants-test",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 

11213c452303a54a25f12a4714fa39cafdac3251
AustinT33 <austintumlinson@gmail.com>
2020-01-11 18:49:25 -0600

    testing

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/api/restaurants',
+    //change DB_URL to DATABASE_URL for development, change back to DB_URL for production
+    DB_URL: process.env.DATABASEB_URL || 'postgresql://postgres@localhost/api/restaurants',
     TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://postgres@localhost/restaurants-test",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 

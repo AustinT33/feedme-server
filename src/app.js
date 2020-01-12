5e5d200748cc1f16125f1e0178c3c809b7520cd3
AustinT33 <austintumlinson@gmail.com>
2020-01-11 19:01:04 -0600

    testing

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 
 app.use('/api/restaurants', RestaurantsRouter)
 app.use('/api/favorites', FavoritesRouter)
